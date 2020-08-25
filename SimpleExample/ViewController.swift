bbf04f971c9d39648f453dbba88a3f4788320721
Neeraj <agnihotrineeraj00@gmail.com>
2020-08-25 21:50:37 +0530

    Initial Commit

---
+++ SimpleExample/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SimpleExample
+//
+//  Created by mac on 25/08/20.
+//  Copyright © 2020 mac. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
